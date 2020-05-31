bd374ee218c0fdcf7ad3eb8e20c128311a8477d1
KihwanPark <zach.san1986@gmail.com>
2020-06-01 00:55:54 +0900

    first commit

---
+++ HelloWorld/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by KIHWAN PARK on 2020/05/31.
+//  Copyright © 2020 KIHWAN PARK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    //click event
+    @IBAction func Click_moveBtn(_ sender: Any) {
+        print("Click MOVE")
+    }
+}
+
